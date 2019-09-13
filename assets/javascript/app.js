e06bc700d6fca89bc136a7ae512af6bc03237294
James Robbins <jameslrobbins86@gmail.com>
2019-09-13 12:52:00 -0500

    Working Quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
+//Questions Array
+
 var quiz = [
+
     {
         question: "What type of scenery do you enjoy?",
         choices: ["Beach", "Mountains", "Urban", "Country", "Small Town"]
@@ -7,11 +10,41 @@ var quiz = [
     {
         question: "Where do you prefer to drink?",
         choices: ["Clubs", "Bars", "Breweries", "Wineries", "Don't Drink"]
+    },
+
+    {
+        question: "What type of music do you prefer?",
+        choices: ["Blues", "Country", "Jazz", "Club", "Rock"]
+    },
+
+    {
+        question: "Where do you like to eat?",
+        choices: ["Fancy restaurants", "Hole-in-the-wall", "Bars", "Chains", "Street Food"]
+    },
+
+    {
+        question: "What sort of transportation do you prefer?",
+        choices: ["Car", "Walk", "Public Transportation", "Bicycle", "Flight"]
+    },
+
+    {
+        question: "What type of attractions do you enjoy?",
+        choices: ["Casinos", "Outdoors", "Museums", "Theme Parks", "Art Galleries"]
+    },
+
+    {
+        question: "What types of weather do you prefer?",
+        choices: ["Sunny", "Rainy", "Winter Wonderland", "Crisp and Cool", "Hot"]
     }
 ]
 
+//Global Variables
+
 var currentQuestion = 0;
 var questions = $("#questions");
+var answersArr = []
+
+//Load Question
 
 function loadQuestion() {
     var question = quiz[currentQuestion].question;
@@ -20,19 +53,49 @@ function loadQuestion() {
     console.log(question)
 }
 
+//Load Choices
+
 function loadChoices(choices) {
     var choices = quiz[currentQuestion].choices;
     
-    $("#choices").append(
-        $("<div class='checkbox'><label><input type='checkbox' value=''> " + quiz[currentQuestion].choices[0] + "</label>"),
-        $("<div class='checkbox'><label><input type='checkbox' value=''> " + quiz[currentQuestion].choices[1] + "</label>"),
-        $("<div class='checkbox'><label><input type='checkbox' value=''> " + quiz[currentQuestion].choices[2] + "</label>"),
-        $("<div class='checkbox'><label><input type='checkbox' value=''> " + quiz[currentQuestion].choices[3] + "</label>"),
-        $("<div class='checkbox'><label><input type='checkbox' value=''> " + quiz[currentQuestion].choices[4] + "</label>"),
-        $("<button type='button' class='btn btn-primary' id='button'> " + "Next Question" + "</button>")
-    )
     
-}   
+    $("#choices").html(
+        $("<div class='checkbox' id='choice1'><label><input type='checkbox' value=" + choices[0] + "> " + choices[0] + "</label>").append(
+        $("<div class='checkbox'><label><input type='checkbox' value=" + choices[1] + "> " + choices[1] + "</label>"),
+        $("<div class='checkbox'><label><input type='checkbox' value=" + choices[2] + "> " + choices[2] + "</label>"),
+        $("<div class='checkbox'><label><input type='checkbox' value=" + choices[3] + "> " + choices[3] + "</label>"),
+        $("<div class='checkbox'><label><input type='checkbox' value=" + choices[4] + "> " + choices[4] + "</label>"),
+        $("<button type='button' class='btn btn-primary' id='button'> " + "Next Question" + "</button>")
+    ))
+
+    //Button Click
+
+    $("#button").on("click", function() {
+
+        $.each($("input[value]:checked"), function() {
+            answersArr.push($(this).val());
+
+            nextQuestion();
+            console.log(answersArr)
+        })
+    })
+    
+};
+
+//Next Question
+
+function nextQuestion() {
+    var quizOver = (quiz.length - 1)  === currentQuestion;
+    if (quizOver) {
+        quizEnd();
+    } else {
+        currentQuestion++;
+        loadQuestion();
+
+    }
+}
+
+//Start Quiz
 
 $("#start").click(function() {
     $("#start").remove();
@@ -41,4 +104,11 @@ $("#start").click(function() {
 
 console.log(quiz[currentQuestion].choices[0])
 console.log(quiz[currentQuestion].choices[1])
+
+//End Quiz
+
+function quizEnd() {
+    $("#questions").html("Please wait while we work our magic!");
+    $("#choices").remove();
+}
 
