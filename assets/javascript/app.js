c3a7f6c73b3f499f7235c3cf62b19cf1ab0abc27
tardyparty <tardyparty@users.noreply.github.com>
2019-09-14 12:40:13 -0500

    wtf

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -169,6 +169,12 @@ function score(){
 
 
 
+    for (i=0; i < cities.length; i++) {
+        console.log(cities[i]);
+    }
+
+
+
 // bubble sort to rank cities by score
 function sort(cities, score) {
 
