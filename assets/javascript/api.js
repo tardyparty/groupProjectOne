2cfd4b18d16b1927fc8ea988b36dedc9c79ebc76
tardyparty <tardyparty@users.noreply.github.com>
2019-09-14 13:24:34 -0500

    formatting

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,14 +1,16 @@
 // place api call builder here
 var key = "3d2fa81c65msh322e2d31fe3aebdp19814bjsnceae2e75aeae";
-var startURL = " https://apidojo-kayak-v1.p.rapidapi.com/";
+var startURL = "https://apidojo-kayak-v1.p.rapidapi.com/";
+var dates;
+var 
+
+// api builder for home airport code search
 var homeURL = "locations/search?where=";
+// var homeInput = $(input from search box on API questions html);
 
 
 // var to store airport code string of users departure city
 var homeCode = "";
-
-
-// var homeInput = $(input from search box on API questions html);
 
 
 // after saving there home city and dates - pushed to api to find their home airport code
@@ -23,6 +25,7 @@ $(document).on("click", "#***add name here***", function(e) {
     }).then(function(data){
         homeCode = data[0].apicode;
     });
-}
+});
+
 
 
