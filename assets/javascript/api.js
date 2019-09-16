3c63a5661193f37f5946786ff2c8d48432ec7b5c
tardyparty <tardyparty@users.noreply.github.com>
2019-09-15 22:53:48 -0500

    both api calls work but not taking in form val()

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,15 +1,16 @@
 // home city ajax
 // api builder for home airport code search
-var homeInput = $("#homeInput").val().trim();
-var homeURL = "https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=";
+var origin = $("#homeInput").val().trim();
 
 // var to store airport code string of users departure city
 var homeCode = "";
 
+
+// api call for origin airport code
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": homeURL + homeInput,
+	"url": "https://apidojo-kayak-v1.p.rapidapi.com/locations/search?where=" + origin,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "apidojo-kayak-v1.p.rapidapi.com",
@@ -18,22 +19,29 @@ var settings = {
 }
 
 
+// acccepts api form inputs
+$("#apiBtn").on("click", function(e){
+    e.preventDefault();
+    console.log(origin);
+    // console.log(homeURL + origin);
+})
+
 $.ajax(settings).done(function (response) {
     homeCode = response[0].apicode;
-	console.log(response);
+    // console.log(response);
+    console.log(homeCode);
 });
 
 
 // flights ajax
 // builds data object for api call
-requestData.origin1 = homeCode;
-requestData.destination1;
-requestData.departdate1 = dates;
+// requestData.destination1;
+// requestData.departdate1 = dates;
 
 // edit this object according to user inputs
 var requestData = {
-    origin1: "SGN",
-    destination1: "DAD",
+    origin1: origin,
+    destination1: cities[0].code,
     departdate1: "2019-12-01",
     cabin: "e",
     currency: "USD",
@@ -42,18 +50,32 @@ var requestData = {
     };
 
 
-$.ajax({
-    type: "GET",
-    url: "https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session",
-    crossDomain:true,
-    headers: { "x-rapidapi-host": "apidojo-kayak-v1.p.rapidapi.com",
-                "x-rapidapi-key": "3d2fa81c65msh322e2d31fe3aebdp19814bjsnceae2e75aeae" },
-    data: requestData,
-    success: function (response) {
-                console.log(response);
-                // ******* code goes here! ********
-        }
-    });
+
+    $("#apiBtn").on("click", function(e){
+    e.preventDefault();
+    console.log(origin);
+    // console.log(homeURL + origin);
+})
+
+$("#results").on("click", function(e){
+    e.preventDefault();
+
+    $.ajax({
+        type: "GET",
+        url: "https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session",
+        crossDomain:true,
+        headers: { "x-rapidapi-host": "apidojo-kayak-v1.p.rapidapi.com",
+                    "x-rapidapi-key": "3d2fa81c65msh322e2d31fe3aebdp19814bjsnceae2e75aeae" },
+        data: requestData,
+        success: function (response) {
+            console.log(response);
+            console.log("price" + response.cheapestPriceTotal);
+            console.log("airline" + response.airlines[0])
+            // ******* code goes here! ********
+            }
+        });
+})
+
         
       
 
